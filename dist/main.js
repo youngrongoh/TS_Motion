d073bedf583369c141309a6df03d9cb7f9208a6f
YoungRong.Oh <youngrong.oh@gmail.com>
2021-03-02 16:17:16 +0900

    모달 add 버튼누르면 목록에 해당 아이템 추가

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,10 @@ const buttons = document.querySelector('.adds');
 const modal = document.querySelector('#modal');
 const contentLabel = modal.querySelector('.label.content');
 const modalExit = document.querySelector('.exit');
+const modalAdd = modal.querySelector('.add');
+const list = document.querySelector('.list');
+const modalForm = modal.querySelector('.form-box');
+let clickedButton;
 function createContentInput(kind) {
     let input;
     if (kind === 'input') {
@@ -24,8 +28,13 @@ function onButtonClick(event) {
     if (target.tagName !== 'BUTTON') {
         return;
     }
+    const input = contentLabel.lastChild;
+    if (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') {
+        input.remove();
+    }
     const contentTitle = contentLabel.querySelector('.name');
     const text = (_a = target.textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    clickedButton = text;
     switch (text) {
         case 'video':
         case 'image':
@@ -42,9 +51,83 @@ function onButtonClick(event) {
     }
     modal.classList.remove('hidden');
 }
+function exitModal() {
+    modal.classList.add('hidden');
+    modalForm.reset();
+    contentLabel.lastChild.remove();
+}
+function getItemContents(kind, text, content) {
+    let structure;
+    switch (kind) {
+        case 'video':
+            content = `
+        <iframe class="media" src="${content}" frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>`;
+            structure = 'lr';
+            break;
+        case 'image':
+            content = `<img class="media" src="${content}">`;
+            structure = 'lr';
+            break;
+        case 'task':
+            content = `
+        <label class="todo" for="todo">
+          <input type="checkbox" id="todo">
+          <span class="text">${content}</span>
+        </label>`;
+            structure = 'tb';
+            break;
+        case 'note':
+            content = `<p class="text">${content}</p>`;
+            structure = 'tb';
+            break;
+        default:
+            throw new Error(`invalid kind of item: ${kind}`);
+    }
+    return {
+        text,
+        content,
+        structure,
+    };
+}
+function createItem(itemContents) {
+    const item = document.createElement('li');
+    item.setAttribute('class', `item ${itemContents.structure}`);
+    if (itemContents.structure === 'lr') {
+        item.innerHTML = `
+    <div class="content">
+    ${itemContents.content}
+    </div>
+    <p class="text">${itemContents.text}</p>
+    <button class="delete">❌</button>`;
+    }
+    else {
+        item.innerHTML = `
+      <h2 class="title">${itemContents.text}</h2>
+      <div class="content">
+        ${itemContents.content}
+      </div>`;
+    }
+    return item;
+}
+function addItem(event) {
+    event.preventDefault();
+    const text = modalForm[0].value;
+    const content = modalForm[1]
+        .value;
+    if (text === undefined || content === undefined) {
+        alert('내용을 입력해주세요');
+        return;
+    }
+    const itemContents = getItemContents(clickedButton, text, content);
+    const item = createItem(itemContents);
+    list.appendChild(item);
+    exitModal();
+}
 buttons.addEventListener('click', onButtonClick);
 modalExit.addEventListener('click', (event) => {
     event.preventDefault();
-    modal.classList.add('hidden');
-    contentLabel.lastChild.remove();
+    exitModal();
 });
+modalAdd.addEventListener('click', addItem);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
+type ItemKinds = 'video' | 'image' | 'task' | 'note';
+
+type ItemStructure = 'lr' | 'tb';
+
+type ItemContents = {
+  text: string;
+  content: string;
+  structure: ItemStructure;
+};
+
 const buttons = <HTMLDivElement>document.querySelector('.adds');
 const modal = <HTMLElement>document.querySelector('#modal');
 const contentLabel = <HTMLLabelElement>modal.querySelector('.label.content');
 const modalExit = <HTMLElement>document.querySelector('.exit');
+const modalAdd = <HTMLButtonElement>modal.querySelector('.add');
+const list = <HTMLUListElement>document.querySelector('.list');
+const modalForm = <HTMLFormElement>modal.querySelector('.form-box');
+
+let clickedButton: ItemKinds;
 
 function createContentInput(kind: 'input' | 'textarea'): HTMLElement {
   let input: HTMLElement;
@@ -23,8 +38,13 @@ function onButtonClick(event: MouseEvent) {
   if (target.tagName !== 'BUTTON') {
     return;
   }
+  const input = contentLabel.lastChild as HTMLElement;
+  if (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') {
+    input.remove();
+  }
   const contentTitle = <HTMLSpanElement>contentLabel.querySelector('.name');
-  const text = target.textContent?.toLowerCase();
+  const text = target.textContent?.toLowerCase() as ItemKinds;
+  clickedButton = text;
   switch (text) {
     case 'video':
     case 'image':
@@ -42,9 +62,92 @@ function onButtonClick(event: MouseEvent) {
   modal.classList.remove('hidden');
 }
 
+function exitModal() {
+  modal.classList.add('hidden');
+  modalForm.reset();
+  contentLabel.lastChild!.remove();
+}
+
+function getItemContents(
+  kind: ItemKinds,
+  text: string,
+  content: string
+): ItemContents {
+  let structure: ItemStructure;
+  switch (kind) {
+    case 'video':
+      content = `
+        <iframe class="media" src="${content}" frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>`;
+      structure = 'lr';
+      break;
+    case 'image':
+      content = `<img class="media" src="${content}">`;
+      structure = 'lr';
+      break;
+    case 'task':
+      content = `
+        <label class="todo" for="todo">
+          <input type="checkbox" id="todo">
+          <span class="text">${content}</span>
+        </label>`;
+      structure = 'tb';
+      break;
+    case 'note':
+      content = `<p class="text">${content}</p>`;
+      structure = 'tb';
+      break;
+    default:
+      throw new Error(`invalid kind of item: ${kind}`);
+  }
+  return {
+    text,
+    content,
+    structure,
+  };
+}
+
+function createItem(itemContents: ItemContents): HTMLElement {
+  const item = document.createElement('li');
+  item.setAttribute('class', `item ${itemContents.structure}`);
+  if (itemContents.structure === 'lr') {
+    item.innerHTML = `
+    <div class="content">
+    ${itemContents.content}
+    </div>
+    <p class="text">${itemContents.text}</p>
+    <button class="delete">❌</button>`;
+  } else {
+    item.innerHTML = `
+      <h2 class="title">${itemContents.text}</h2>
+      <div class="content">
+        ${itemContents.content}
+      </div>`;
+  }
+  return item;
+}
+
+function addItem(event: MouseEvent) {
+  event.preventDefault();
+
+  const text = (modalForm[0] as HTMLInputElement).value;
+  const content = (modalForm[1] as HTMLInputElement | HTMLTextAreaElement)
+    .value;
+
+  if (text === undefined || content === undefined) {
+    alert('내용을 입력해주세요');
+    return;
+  }
+  const itemContents = getItemContents(clickedButton, text, content);
+  const item = createItem(itemContents);
+  list.appendChild(item);
+  exitModal();
+}
+
 buttons.addEventListener('click', onButtonClick);
 modalExit.addEventListener('click', (event: MouseEvent) => {
   event.preventDefault();
-  modal.classList.add('hidden');
-  contentLabel.lastChild!.remove();
+  exitModal();
 });
+modalAdd.addEventListener('click', addItem);
