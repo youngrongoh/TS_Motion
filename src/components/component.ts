896732f695346ea1e9079dc04f0eab89f31b5c1e
YoungRong.Oh <youngrong.oh@gmail.com>
2021-03-10 02:00:46 +0900

    컴포넌트 리펙토링

---
+++ src/components/component.ts
@@ -0,0 +1,19 @@
+export interface Component {
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+
+/**
+ * HTML 요소 생성을 캡슐화
+ */
+export class BaseComponent<T extends HTMLElement> implements Component {
+  protected readonly element: T;
+
+  constructor(htmlString: string) {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString;
+    this.element = template.content.lastElementChild! as T;
+  }
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
