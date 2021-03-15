63449cc2fbcae655039b4c353281459102015afb
YoungRong.Oh <youngrong.oh@gmail.com>
2021-03-15 18:05:42 +0900

    디펜던시 인젝션 리펙토링

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,17 @@ import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { Composable, PageComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 class App {
   private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent(); // 클래스 안에서 다른 클래스를 만들기 보다 DI를 통해 외부에서 주입 받는 것이 확장성과 유닛테스트에 좋음
+    this.page = new PageComponent(PageItemComponent); // 클래스 안에서 다른 클래스를 만들기 보다 DI를 통해 외부에서 주입 받는 것이 확장성과 유닛테스트에 좋음
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      'Image Title',
-      'https://picsum.photos/600/300'
-    );
+    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
     this.page.addChild(image);
 
-    const video = new VideoComponent(
-      'Video Title',
-      'https://youtu.be/XRIefKC0n2k'
-    );
+    const video = new VideoComponent('Video Title', 'https://youtu.be/XRIefKC0n2k');
     this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'Note Body');
