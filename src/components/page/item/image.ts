3b3b3600d152af2f30e0fd48a338c26142dc6881
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-01 00:38:06 +0900

    feat: Base 컴포넌트 수정

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,17 +1,13 @@
-import { BaseComponent } from '../base.js';
+import { BaseComponent } from '../component.js';
 
-export class ImageComponent extends BaseComponent {
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const stringTeamplate = `
-    <section class="image">
-      <div class="image__holder">
-        <img class="image__thumbnail">
-      </div>
-      <p class="image__title"></p>
-    </section>
-    `;
-
-    super(stringTeamplate);
+    super(`<section class="image">
+            <div class="image__holder">
+              <img class="image__thumbnail">
+            </div>
+            <p class="image__title"></p>
+          </section>`);
 
     const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
     imageElement.src = url;
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,9 +1,7 @@
-import { BaseComponent } from './base.js';
+import { BaseComponent } from './component.js';
 
-export class PageComponent extends BaseComponent {
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    const stringTeamplate = `<ul class="page">페이지 컴포넌트가 추가되었습니다.</ul>`;
-
-    super(stringTeamplate);
+    super('<ul class="page">페이지 컴포넌트가 추가되었습니다.</ul>');
   }
 }
