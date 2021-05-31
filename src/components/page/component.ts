a696a7c4d53829595e9fe6ed75b6a5d367ea8b06
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-01 00:32:28 +0900

    feat: Base  컴포넌트 추가

---
+++ src/components/page/component.ts
@@ -0,0 +1,21 @@
+export interface Component {
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+
+/**
+ * HTML element 생성을 캡슐화 함
+ */
+
+export class BaseComponent<T extends HTMLElement> implements Component {
+  protected readonly element: T;
+
+  constructor(htmlString: string) {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString;
+    this.element = template.content.firstElementChild! as T;
+  }
+
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
