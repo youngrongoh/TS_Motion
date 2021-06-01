68088e28484b87bda927752e770ec68b7f66aeb7
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-01 09:51:53 +0900

    feat: Base 컴포넌트 element 속성 접근제어자 변경

--- src/components/page/component.ts
+++ src/components/page/component.ts
@@ -3,7 +3,7 @@ export interface Component {
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
-  public element: T;
+  protected element: T;
 
   constructor(htmlString: string) {
     const template = document.createElement('template');
