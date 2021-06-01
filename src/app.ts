29c0b8d931664cd9165574a99b87602b607a2c1f
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-01 13:40:50 +0900

    feat: video 컴포넌트 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -17,6 +18,9 @@ class App {
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
     todo.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent('Video Title', '');
+    video.attachTo(appRoot, 'beforeend');
   }
 }
 
