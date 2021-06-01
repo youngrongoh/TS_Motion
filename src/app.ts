68c7a8bc75d9d51bcfe75679e380f80043f607c4
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-01 17:23:17 +0900

    feat: page item 컨테이너 추가

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
+import { Component } from './components/page/component.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
 
-    const video = new VideoComponent('Video Title', '');
-    video.attachTo(appRoot, 'beforeend');
+    const video = new VideoComponent(
+      'Video Title',
+      'https://www.youtube.com/embed/M7lc1UVf-VE'
+    );
+    this.page.addChild(video);
   }
 }
 
